b802feabd9651b083197471f401742d0b1719b1b
thayour <temyking@gmail.com>
2024-10-08 13:09:04 +0100

    work

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,30 +8,30 @@ export default function Foooter() {
 
     return (
         <div className="mt-10 text-white  w-full p-4 max-w-screen-2xl mx-auto xl:px-24 px-10">
-            <footer className="footer bg-neutral text-neutral-content items-center p-4">
+            <footer className="footer text-neutral-content items-center p-4">
                 <aside className="grid-flow-col items-center">
                     <Link to='/' className='text-xl font-bold text-purple-700 flex
                         items-center'>
-                        <img className='img' src={img} alt="" />
-                        <p>Job <span className='text-white'>Portal</span></p>
+                        <img className='img rounded-full' src={img} alt="" />
+                         <span className='text-white'>thayour</span>
                     </Link>
                     <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
                 </aside>
                 <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
                     <a>
                         <a href="https://github.com/thayour02">
-                            <FaGithub />
+                            <FaGithub size={40} />
                         </a>
                     </a>
                     <a>
                         <a href="https://www.linkedin.com/in/usman-temytayo-85a75329a"
                         >
-                      <FaLinkedin/>
+                      <FaLinkedin size={40}/>
                         </a>
                     </a>
                     <a>
                         <a href="mailto:temyking@gmail.com.com"></a>
-                        <HiOutlineMail />
+                        <HiOutlineMail size={40} />
                     </a>
                 </nav>
             </footer>
